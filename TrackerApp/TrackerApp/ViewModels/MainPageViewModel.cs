4dffadd9a4c73124cb2a8496cfd70d5b8b19c559
Michał Pogorzelski <michal.pogorzelski@thetrainline.com>
2018-10-17 19:18:24 +0200

    Conflict

--- TrackerApp/TrackerApp/ViewModels/MainPageViewModel.cs
+++ TrackerApp/TrackerApp/ViewModels/MainPageViewModel.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Threading.Tasks;
 using Plugin.Geolocator;
 using Plugin.Geolocator.Abstractions;
-using TrackerApp.BackgroundProcessing;
 using Xamarin.Forms;
 using Xamarin.Forms.GoogleMaps;
 using Xamarin.Forms.GoogleMaps.Bindings;
-using MessagingCenter = Xamarin.Forms.MessagingCenter;
 using Position = TrackerApp.Models.Position;
 
 namespace TrackerApp
@@ -20,20 +17,31 @@ namespace TrackerApp
 
         private ObservableCollection<Position> _positions;
         private readonly IGeolocator _geolocator;
+        private bool _canDisplay;
 
         public MainPageViewModel()
         {
             _geolocator = CrossGeolocator.Current;
             StartTrackingCommand = new RelayCommand(StartTracking);
             StopTrackingCommand = new RelayCommand(StopTracking);
-
-            HandleReceivedMessages();
         }
 
         public RelayCommand StartTrackingCommand { get; }
         public RelayCommand StopTrackingCommand { get; }
 
+        public bool CanDisplay
+        {
+            get => _canDisplay;
+            private set
+            {
+                _canDisplay = value;
+                OnPropertyChanged(nameof(CanDisplay));
+            }
+        }
+
         public MoveCameraRequest MoveCameraRequest { get; set; } = new MoveCameraRequest();
+
+        public ObservableCollection<Circle> Circles { get; set; } = new ObservableCollection<Circle>();
 
         public ObservableCollection<Polyline> Polylines { get; set; } = new ObservableCollection<Polyline>();
 
@@ -52,8 +60,20 @@ namespace TrackerApp
 
         private async void StartTracking()
         {
-            if (await ValidateGeolocationPermission())
+            Console.WriteLine("####### Checking location is enabled.");
+            if (!_geolocator.IsGeolocationEnabled)
             {
+                await DisplayAlert("Location", "Please turn on location services.", "Ok");
+                return;
+            }
+
+            var result = await Plugin.Permissions.CrossPermissions.Current.RequestPermissionsAsync(new[] { Plugin.Permissions.Abstractions.Permission.Location });
+            var status = result[Plugin.Permissions.Abstractions.Permission.Location];
+
+            Console.WriteLine("####### Checking permissions.");
+            if (status != Plugin.Permissions.Abstractions.PermissionStatus.Granted)
+            {
+                await DisplayAlert("Location", "Location is required for GPS. Device data cannot be saved.", "Cancel");
                 return;
             }
 
@@ -62,43 +82,29 @@ namespace TrackerApp
                 return;
             }
 
-            var message = new StartTrackingTaskMessage();
-            MessagingCenter.Send(message, nameof(StartTrackingTaskMessage));
+            CanDisplay = false;
+            this.Positions = new ObservableCollection<Position>();
+            await _geolocator.StartListeningAsync(TimeSpan.FromSeconds(2), 0);
+            _geolocator.PositionChanged += PositionChanged;
         }
 
-        private void StopTracking()
+        private async void StopTracking()
         {
             if (!_geolocator.IsListening)
             {
                 return;
             }
 
-            var message = new StopTrackingTaskMessage();
-            MessagingCenter.Send(message, nameof(StopTrackingTaskMessage));
-        }
+            await _geolocator.StopListeningAsync();
+            _geolocator.PositionChanged -= PositionChanged;
 
-        private async Task<bool> ValidateGeolocationPermission()
-        {
-            Console.WriteLine("####### Checking location is enabled.");
-            if (!_geolocator.IsGeolocationEnabled)
+            CanDisplay = true;
+            if (Positions.Count < 2)
             {
-                await DisplayAlert("Location", "Please turn on location services.", "Ok");
-                return true;
+                return;
             }
 
-            var result =
-                await Plugin.Permissions.CrossPermissions.Current.RequestPermissionsAsync(new[]
-                    {Plugin.Permissions.Abstractions.Permission.Location});
-            var status = result[Plugin.Permissions.Abstractions.Permission.Location];
-
-            Console.WriteLine("####### Checking permissions.");
-            if (status != Plugin.Permissions.Abstractions.PermissionStatus.Granted)
-            {
-                await DisplayAlert("Location", "Location is required for GPS. Device data cannot be saved.", "Cancel");
-                return true;
-            }
-
-            return false;
+            DrawPolyline(Positions.ToList());
         }
 
         private void PositionChanged(object sender, PositionEventArgs args)
@@ -130,35 +136,6 @@ namespace TrackerApp
             (CameraUpdateFactory.NewCameraPosition(
                 new CameraPosition(
                     new Xamarin.Forms.GoogleMaps.Position(polyline.Positions.First().Latitude, polyline.Positions.First().Longitude), 9d)));
-        }
-
-        private void HandleReceivedMessages()
-        {
-            MessagingCenter.Subscribe<NewPositionMessage>(this, nameof(NewPositionMessage), message =>
-            {
-                Device.BeginInvokeOnMainThread(async () =>
-                {
-                    Positions = new ObservableCollection<Position>();
-                    await _geolocator.StartListeningAsync(TimeSpan.FromSeconds(2), 0);
-                    _geolocator.PositionChanged += PositionChanged;
-                });
-            });
-
-            MessagingCenter.Subscribe<CancelledMessage>(this, nameof(CancelledMessage), message =>
-            {
-                Device.BeginInvokeOnMainThread(async () =>
-                {
-                    await _geolocator.StopListeningAsync();
-                    _geolocator.PositionChanged -= PositionChanged;
-
-                    if (Positions.Count < 2)
-                    {
-                        return;
-                    }
-
-                    DrawPolyline(Positions.ToList());
-                });
-            });
         }
     }
 }
